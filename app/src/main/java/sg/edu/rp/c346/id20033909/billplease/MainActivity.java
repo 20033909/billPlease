1bef11c096a6f996979069ef077e88a9706389c5
jlphoenix9 <20033909@myrp.edu.sg>
2023-05-04 20:18:05 +0800

    AAAARGHHHH

--- app/src/main/java/sg/edu/rp/c346/id20033909/billplease/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20033909/billplease/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        = findViewById(R.id.EditAmt);
+        = findViewById(R.id.editAmt);
         = findViewById(R.id.textPax);
         = findViewById(R.id.toggSv);
         = findViewById(R.id.toggGst);
